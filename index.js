ca29979355e4d85f986aa60e8af72ad99d41dc2f
greatdane <vanderbyljeandre@gmail.com>
2023-07-29 16:49:38 +0200

    working

---
+++ index.js
@@ -0,0 +1,11 @@
+// template strings/literals
+
+const recipient = "James"
+const sender = "Jeandre"
+// Create a new variable, sender, and set its value to your name
+
+// Use your sender variable instead of "Per"
+// Refactor the email string to use template strings
+const email = `Hey ${recipient}! How is it going? Cheers ${sender}`
+
+console.log(email)
